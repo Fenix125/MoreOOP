fae3034e1b6272c63475e418251dc9103538b514
Fenix125 <migelspainn@gmail.com>
2024-09-29 18:10:40 +0300

    fixing random error in GameManager

--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -2,9 +2,8 @@ package lotr;
 import java.util.Random;
 public class GameManager {
     public void fight(Character c1, Character c2){
-        Random ran = new Random();
         while (c1.getHp() > 0 && c2.getHp() > 0) {
-            int turn = ran.nextInt(2);
+            int turn = new Random().nextInt(2);
             if (turn == 0){
                 System.out.println(c1 + " attacks: " + c2);
                 c1.kick(c2);
