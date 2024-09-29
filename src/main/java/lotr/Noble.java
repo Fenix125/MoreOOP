3cf465879821dd04b4bec65962e97faeb35fcbc8
Fenix125 <migelspainn@gmail.com>
2024-09-29 18:29:19 +0300

    fixed error in noble due to random

--- src/main/java/lotr/Noble.java
+++ src/main/java/lotr/Noble.java
@@ -4,6 +4,6 @@ import java.util.Random;
 
 public abstract class Noble extends Character{
     public Noble(int min, int max){
-        super(new Random().nextInt(min, max), new Random().nextInt(min, max), new NobleKick());
+        super(new Random().nextInt(max - min + 1) + min, new Random().nextInt(max - min + 1) + min , new NobleKick());
     }
 }
