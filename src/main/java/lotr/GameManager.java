f8e971db1ff5350e29b0c903e41f10b71ca977bf
Fenix125 <migelspainn@gmail.com>
2024-09-29 18:00:11 +0300

    fixing GameManager

--- src/main/java/lotr/GameManager.java
+++ src/main/java/lotr/GameManager.java
@@ -4,7 +4,7 @@ public class GameManager {
     public void fight(Character c1, Character c2){
         Random ran = new Random();
         while (c1.getHp() > 0 && c2.getHp() > 0) {
-            int turn = ran.nextInt(0, 2);
+            int turn = ran.nextInt(2);
             if (turn == 0){
                 System.out.println(c1 + " attacks: " + c2);
                 c1.kick(c2);
